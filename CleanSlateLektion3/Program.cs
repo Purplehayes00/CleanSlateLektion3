57cc57e3fa5bcc4776d2797fd21fdb2b831b8fac
Johan Rohdin <Johanrohdin@hotmail.com>
2020-11-18 11:51:24 +0100

    MyFirst Git Commit

--- CleanSlateLektion3/Program.cs
+++ CleanSlateLektion3/Program.cs
@@ -9,16 +9,50 @@ namespace CleanSlateLektion3
         {
             //Övning två
 
-            int[] firstOne = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-           // int[] secondOne = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            // int[] firstOne = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // int[] secondOne = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
-           // Console.WriteLine(string.Join(',', firstOne));
+            // Console.WriteLine(string.Join(',', firstOne));
             // Console.WriteLine(string.Join(',', secondOne));
 
             //Ett annat sätt för samma
 
-            Console.WriteLine(string.Join(',', firstOne));
-            Console.WriteLine(string.Join(',', firstOne.Reverse));
+            //Console.WriteLine(string.Join(',', firstOne));
+            //Console.WriteLine(string.Join(',', firstOne.Reverse));
+            //int a;
+            //int yes;
+
+            string[] stringArray = new string[]
+                {"January" +
+                "Febuary" +
+                "mars" +
+                "april" +
+                "maj" +
+                "juni" +
+                "juli" +
+                "augusti" +
+                "september" +
+                "oktober" +
+                "november" +
+                "december" 
+                 };
+            string number;
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Skriv ett nummer från 1-12");
+                number = Console.ReadLine();
+                choice = Convert.ToInt32(number);
+                if (choice <=12)
+                {
+                    Console.WriteLine(stringArray[choice -1]);
+                }
+
+            } while (choice < 12);
+
+
+ 
         }
     }
 }
